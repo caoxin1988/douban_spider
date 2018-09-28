c23c1b030b13fd6804e81900471a5aaa5a4afb45
unknown <caoxin@funshion.com>
2018-09-28 17:56:13 +0800

    get more comments with film id
    Signed-off-by: unknown <caoxin@funshion.com>

--- douban.py
+++ douban.py
@@ -1,8 +1,13 @@
 import requests
 from pyquery import PyQuery as pq
 import json
+import re
 
 URL_MAIN = 'https://movie.douban.com/'
+headers = {
+        'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 \
+                        (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36'
+}
 
 def get_hrefs(items):
     dict_href = {}
@@ -22,12 +27,33 @@ def get_hrefs(items):
     return dict_href
 
 
+def get_comments(film_id):
+    url = 'https://movie.douban.com/subject/26336252/?from=showing'
+    url = URL_MAIN + 'subject/' + film_id + '/'
+    params = {
+        'from' : 'showing'
+    }
+
+    response = requests.get(url=url, params=params, headers=headers)
+
+    doc = pq(response.text)
+    html = doc('.article #comments-section #hot-comments a')
+    for item in html.items():
+        if item.attr('href').startswith('comments'):
+            pattern = re.compile('comments.sort=(?P<sort>.*?)&status=(?P<status>.*?)$')
+            res = re.match(pattern, item.attr('href'))
+            params = res.groupdict()
+            break
+
+    url = URL_MAIN + 'subject/' + film_id + '/comments/'
+    response = requests.get(url=url, params=params, headers=headers)
+
+    with open('test', 'w', encoding='utf-8') as f:
+        f.write(response.text)
+
+
 def main():
     url = URL_MAIN
-    headers = {
-        'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 \
-                        (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36'
-    }
 
     r = requests.get(url=url, headers=headers)
 
@@ -39,6 +65,14 @@ def main():
     for key in dict_films.keys():
         if '碟中谍' in key:
             print(dict_films.get(key))
+            film_url = dict_films.get(key)
+
+    # get the file id
+    pattern = re.compile('https:.*/(\d+?)/')
+    result = re.match(pattern, film_url)
+    film_id = result.group(1)
+
+    get_comments(film_id)
 
 if __name__ == '__main__':
     main()
