0a0d81671c93fcb68893eb14bb9844ea02d01536
caoxin <caoxin1988s@gmail.com>
2018-12-25 02:19:28 +0800

    add FilmId.py for film id
    Signed-off-by: caoxin <caoxin1988s@gmail.com>

---
+++ FilmId.py
@@ -0,0 +1,5 @@
+from enum import Enum, unique
+
+@unique
+class FilmId(Enum):
+    PADMAN = '27198855'
--- douban.py
+++ douban.py
@@ -2,6 +2,9 @@ import requests
 from pyquery import PyQuery as pq
 import json
 import re
+import sys
+
+from FilmId import FilmId
 
 URL_MAIN = 'https://movie.douban.com/'
 headers = {
@@ -9,6 +12,7 @@ headers = {
                         (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36'
 }
 
+'''
 def get_hrefs(items):
     dict_href = {}
     for li in items.items():
@@ -25,10 +29,9 @@ def get_hrefs(items):
     
     print(dict_href.__len__())
     return dict_href
-
+'''
 
 def get_comments(film_id):
-    url = 'https://movie.douban.com/subject/26336252/?from=showing'
     url = URL_MAIN + 'subject/' + film_id + '/'
     params = {
         'from' : 'showing'
@@ -38,6 +41,9 @@ def get_comments(film_id):
 
     doc = pq(response.text)
     html = doc('.article #comments-section #hot-comments a')
+    print(html)
+    return
+
     for item in html.items():
         if item.attr('href').startswith('comments'):
             pattern = re.compile('comments.sort=(?P<sort>.*?)&status=(?P<status>.*?)$')
@@ -55,24 +61,7 @@ def get_comments(film_id):
 def main():
     url = URL_MAIN
 
-    r = requests.get(url=url, headers=headers)
-
-    html = pq(r.text)
-    lis = html('#screening .screening-bd .ui-slide-content li.ui-slide-item')
-
-    dict_films = get_hrefs(lis)
-    
-    for key in dict_films.keys():
-        if '碟中谍' in key:
-            print(dict_films.get(key))
-            film_url = dict_films.get(key)
-
-    # get the file id
-    pattern = re.compile('https:.*/(\d+?)/')
-    result = re.match(pattern, film_url)
-    film_id = result.group(1)
-
-    get_comments(film_id)
+    get_comments(FilmId.PADMAN.value)
 
 if __name__ == '__main__':
     main()
