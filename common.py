7625ec9c8050f0fd14e1afd2788aa3a3e93ea3ab
caoxin <caoxin1988s@gmail.com>
2018-12-26 00:05:30 +0800

    add login.py, but need to debug it
    Signed-off-by: caoxin <caoxin1988s@gmail.com>

--- common.py
+++ common.py
@@ -1,3 +1,4 @@
+LOGIN_URL = 'https://accounts.douban.com/login'
 
 URL_MAIN = 'https://movie.douban.com/'
 DOUBAN_COMMENTS = 'comments/'
@@ -9,3 +10,6 @@ HEADERS = {
         'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 \
                         (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36'
 }
+
+USER_NAME = 'caoxin1988@yeah.net'
+PASS_WORD = 'caoxin19881227'
