e393b5f497312bc382d7bb4ae56909ef343bfbe0
caoxin <caoxin1988s@gmail.com>
2018-12-25 16:26:22 +0800

    success to get comment info, but here we have a problem, we need to login douban first
    Signed-off-by: caoxin <caoxin1988s@gmail.com>

---
+++ common.py
@@ -0,0 +1,11 @@
+
+URL_MAIN = 'https://movie.douban.com/'
+DOUBAN_COMMENTS = 'comments/'
+DOUBAN_SUBJECT = 'subject/'
+
+RESULT_FILE = 'result.txt'
+
+HEADERS = {
+        'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 \
+                        (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36'
+}
--- douban.py
+++ douban.py
@@ -3,14 +3,10 @@ from pyquery import PyQuery as pq
 import json
 import re
 import sys
+import csv
 
 from FilmId import FilmId
-
-URL_MAIN = 'https://movie.douban.com/'
-headers = {
-        'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 \
-                        (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36'
-}
+import common
 
 '''
 def get_hrefs(items):
@@ -31,37 +27,92 @@ def get_hrefs(items):
     return dict_href
 '''
 
-def get_comments(film_id):
-    url = URL_MAIN + 'subject/' + film_id + '/'
+def get_comment_page(film_id):
+    film_id += '/'
+    url = common.URL_MAIN + common.DOUBAN_SUBJECT + film_id
     params = {
         'from' : 'showing'
     }
 
-    response = requests.get(url=url, params=params, headers=headers)
+    response = requests.get(url=url, params=params, headers=common.HEADERS)
+    with open('test.txt', 'w', encoding='utf-8') as f:
+        f.write(response.text)
 
     doc = pq(response.text)
     html = doc('.article #comments-section #hot-comments a')
-    print(html)
-    return
 
+    count = 0
     for item in html.items():
         if item.attr('href').startswith('comments'):
-            pattern = re.compile('comments.sort=(?P<sort>.*?)&status=(?P<status>.*?)$')
+            pattern = re.compile('comments.*sort=(?P<sort>.*?)&.*status=(?P<status>.*)$')
             res = re.match(pattern, item.attr('href'))
             params = res.groupdict()
+
+            pattern = re.compile('\d+')
+            res = re.findall(pattern, item.text())
+            if res:
+                count = res[0]
             break
 
-    url = URL_MAIN + 'subject/' + film_id + '/comments/'
-    response = requests.get(url=url, params=params, headers=headers)
+    return film_id, params, count
 
-    with open('test', 'w', encoding='utf-8') as f:
-        f.write(response.text)
+def get_comment_percent(doc : pq):
+    '''
+    get comment percent for target film
+    '''
 
+    html = doc('.comment-filter')
+    comment_name = html.find('.filter-name').text().split(' ')[1:]
+    comment_percent = html.find('.comment-percent').text().split(' ')[1:]
+
+    print(comment_name, comment_percent)
+
+
+def get_comment_content(doc : pq, csv_writer):
+    '''
+    get comment content for target film
+    '''
+    comments = doc('.comment')
+    for comment in comments.items():
+        votes = comment('.votes').text()
+        user_name = comment('.comment-info a').text()
+        content = comment('.short').text()
+        
+        csv_writer.writerow([user_name, votes, content])
+
+def get_comments(film_id, param, csv_writer):
+
+    film_id += '/'
+    url = common.URL_MAIN + common.DOUBAN_SUBJECT + film_id + common.DOUBAN_COMMENTS
+    response = requests.get(url=url, params=param, headers=common.HEADERS)
+
+    doc = None
+    if 'start' in param:
+        html = json.loads(response.text)['html']
+        doc = pq(html)
+    else:
+        doc = pq(response.content)
+
+    if 'start' not in param:
+        get_comment_percent(doc)
+
+    get_comment_content(doc, csv_writer)
 
 def main():
-    url = URL_MAIN
+    file_id, param, comment_count = get_comment_page(FilmId.PADMAN.value)
 
-    get_comments(FilmId.PADMAN.value)
+    with open(common.RESULT_FILE, 'w', encoding='utf-8') as f:
+        csv_writer = csv.writer(f)
+
+        for i in range(int(comment_count) // 20 + 1):
+            print(i)
+            if i == 0:
+                get_comments(file_id, param, csv_writer)
+            else:
+                param['start'] = str(i * 20)
+                param['limit'] = '20'
+                param['comments_only'] = '1'
+                get_comments(file_id, param, csv_writer)
 
 if __name__ == '__main__':
     main()
